35368bcbf54f3bc7b8447613070f959b7c97a419
MiaV14 <55615456+MiaV14@users.noreply.github.com>
2025-11-21 10:48:20 -0800

    start main page implementation
    - added home page layout with product listings
    - added placeholder images for banner and products

--- app/frontend/src/pages/Home.jsx
+++ app/frontend/src/pages/Home.jsx
@@ -1,5 +1,55 @@
+import banner_image from '../assets/placeholder.jpg';
+import product_image from '../assets/product_placeholder.png';
+
+const ProductListing = ({ name, image, description, price }) => {
+    return <div className="flex flex-row rounded-sm border-2 border-secondary gap-4 bg-white">
+        <div className="flex flex-col font-sans p-2">
+            <h2 className="text-2xl font-bold">{name}</h2>
+            <p>{description}</p>
+            <div>
+                <p className="text-2xl font-bold">{price}</p>
+            </div>
+        </div>
+        <img src={image} style={{height: 200, width: 200}} alt="placeholder"></img>
+    </div>
+}
+
 const Home = () => {
-    return <div>Home Page</div>
+    const products = [
+        {
+            name: "Product 1",
+            image: product_image,
+            description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+            price: "$0.00"
+        },
+        {
+            name: "Product 2",
+            image: product_image,
+            description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+            price: "$0.00"
+        },
+        {
+            name: "Product 3",
+            image: product_image,
+            description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+            price: "$0.00"
+        },
+        {
+            name: "Product 4",
+            image: product_image,
+            description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+            price: "$0.00"
+        }
+    ]
+    return <div>
+        <img style={{height: 300}} src={banner_image} alt="placeholder"></img>
+        <div className="flex flex-col gap-4 bg-primary-light mt-8 p-4">
+        <h1 className="text-4xl text-center font-bold">Home Page</h1>
+        <div className="flex flex-col md:grid gap-4 grid-cols-2">{products.map((product, index) => {
+            return <ProductListing name={product.name} image={product.image} description={product.description} price={product.price}/>
+        })}</div>
+        </div>
+        </div>
 }
 
 export default Home
