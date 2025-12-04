1eef47861794400aa88af952f65eeb6f22a8bb45
Theoden Melgar <theodenmelgar@gmail.com>
2025-12-03 19:37:11 -0800

    deleted misplaced products directory

--- app/backend/products/products.routes.js
+++
@@ -1,8 +0,0 @@
-import { Router } from 'express';
-import { getAllProducts } from './products.controllers.js';
-
-const productsRouter = Router()
-
-productsRouter.get('/products', getAllProducts);
-
-export default productsRouter
