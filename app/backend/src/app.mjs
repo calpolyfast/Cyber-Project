509834134ca408f5212ebb498f72787d250d79b9
Jonathan Woline <jonwoline@gmail.com>
2025-12-19 01:07:42 -0800

    Added CORS

--- app/backend/src/app.mjs
+++ app/backend/src/app.mjs
@@ -1,18 +1,21 @@
 import express from 'express'
 import dotenv from 'dotenv'
 import prisma from './config/db.js'
+import cors from 'cors'
+import options from './cors_options.js'
 
 import UserRouter from './users/users.routes.js'
 import VulnerabilityGroupRouter from './vulnerabilityGroups/vulnerabilityGroups.routes.js'
 import VulnerabilityRouter from './vulnerabilities/vulnerabilities.routes.js'
 import ProductRouter from './products/products.routes.js';
 
-dotenv.config()
+dotenv.config();
 
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(express.json())
+app.use(cors(options))
 
 app.use('/users', UserRouter)
 app.use('/products', ProductRouter)
