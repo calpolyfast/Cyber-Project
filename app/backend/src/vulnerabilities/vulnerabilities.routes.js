85ad9cf5d63cfb9ba9fe321b0204b68224273f71
Jonathan Woline <90117345+Literally-Code@users.noreply.github.com>
2025-12-18 23:59:08 -0800

    Merge /backend with main (#11)
    * added prisma config and an example route to display users
    
    * added basic vulnerability and vulnerability gruop features
    
    * added tests for login controller and register controller. tests include checks for unique usernames, invalid usernames and passwords (blank fields), and proper hasing of password
    
    * added tests for product controllers. tests include checks for valid request bodies and graceful error handling in cases where the request body may be invalid
    
    * added order and order items tests. includes tests for creating an order and viewing past orders for a user
    
    * Products (#8)
    
    * Added raglem to CODEOWNERS
    
    * added new java script file
    
    * began writing routes/schema
    
    * minor fixes placing controllers in right file. also fixed naming of controllers to 'products'
    
    * fixed some errors in product tests
    
    * fixed issue mocking prisma product call
    
    * fixed errors
    
    * added searchProductByName controller and simplified products fields in controllers
    
    * deleted misplaced products directory
    
    ---------
    
    Co-authored-by: Jonathan Woline <jonwoline@gmail.com>
    Co-authored-by: Logan Quon <Logan>
    
    * Products (#9)
    
    * Added raglem to CODEOWNERS
    
    * added new java script file
    
    * began writing routes/schema
    
    * minor fixes placing controllers in right file. also fixed naming of controllers to 'products'
    
    * fixed some errors in product tests
    
    * fixed issue mocking prisma product call
    
    * fixed errors
    
    * added searchProductByName controller and simplified products fields in controllers
    
    * deleted misplaced products directory
    
    * added multer middleware and cloudinary image uploading in createProduct controller
    
    * added PUT api route. the route also supports deleting the old image and uploading the new image to Cloudinary
    
    ---------
    
    Co-authored-by: Jonathan Woline <jonwoline@gmail.com>
    Co-authored-by: Logan Quon <Logan>
    
    * Gf routecreate person (#10)
    
    * Added raglem to CODEOWNERS
    
    * Created a createPerson route
    
    * Possible fix?
    
    * consolidated new User logic inside users feature folder. also added router to app.js
    
    * fixed user tests to properly mock bcrypt and jsonwebtoken modules
    
    * added middleware and jwt token generation to handle authenticated requests
    
    ---------
    
    Co-authored-by: Jonathan Woline <jonwoline@gmail.com>
    Co-authored-by: Gabriel Fernandez <gmoney04626@gmail.com>
    
    * added middleware protected routes to products
    
    ---------
    
    Co-authored-by: Theoden Melgar <theodenmelgar@gmail.com>
    Co-authored-by: Theoden Melgar <113568468+raglem@users.noreply.github.com>
    Co-authored-by: Gabriel Fernandez <gmoney04626@gmail.com>

--- app/backend/src/vulnerabilities/vulnerabilities.routes.js
+++ app/backend/src/vulnerabilities/vulnerabilities.routes.js
@@ -4,11 +4,16 @@ import
     createVulnerability, deleteVulnerability, 
     getAllVulnerabilities, getVulnerabilityById 
 } from "./vulnerabilities.controllers.js";
+import verifyUser from "../middleware/verifyUser.js";
+import verifyOwner from "../middleware/verifyOwner.js";
 
 const router = Router()
 
+router.use(verifyUser)
 router.get('/', getAllVulnerabilities)
 router.get('/:id', getVulnerabilityById)
+
+router.use(verifyOwner)
 router.post('/', createVulnerability)
 router.delete('/:id', deleteVulnerability)
 
