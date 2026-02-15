32fe67ef9fc92ead20ebd86c8ad4f04bdaa940e3
Logan Quon <Logan Quon>
2026-02-15 13:44:41 -0800

    updated account routes

--- app/backend/src/accounts/accounts.controllers.js
+++ app/backend/src/accounts/accounts.controllers.js
@@ -4,6 +4,7 @@ export const getAllAccounts = async (req, res) => {
     try {
         const accounts = await prisma.accounts.findMany();
         res.json(accounts);
+        
     }
     catch (error) {
         res.status(400).json({ error: 'Failed to fetch accounts' });
@@ -12,5 +13,28 @@ export const getAllAccounts = async (req, res) => {
 
 export const getProfile = async (req, res) => {
     try {
-        const profile = await prisma.accounts.findUnique();
+        const profile = await prisma.accounts.findUnique({
+            where: { id: Number(id)} ,
+            data: {email: String(email)},
+            data: {password: String(password)}
+        });
+        res.json(profile);
     }
+    catch (error) {
+        res.status(404).json({error: 'Profile not found.'});
+    }
+};
+
+export const deleteAccount = async (req, res) => {
+    try {
+        const currentAccount = await prisma.accounts.findUnique({
+            where: {id: Number(id)},
+            data: {email: String(email)},
+            data: {password: String(password)}
+        });
+        res.status(200).json({res: currentAccount + 'Account successfully deleted'})
+    }
+    catch (error) {
+        res.status(404).json({error: 'Profile not found.'});
+    }
+}
--- app/backend/src/accounts/accounts.routes.js
+++ app/backend/src/accounts/accounts.routes.js
@@ -1,12 +1,13 @@
 import {
     getAllAccounts, getProfile,
-    deleteAccount
+    deleteAccount, createAccount
 } from './accounts.controllers.js';
 import { Router } from 'express';
 
 const accountsRouter = Router()
 accountsRouter.get('/', getAllAccounts);
 accountsRouter.get('/profile', getProfile);
+accountsRouter.delete('/delete', deleteAccount);
 
 
 
