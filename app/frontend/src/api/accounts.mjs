f6281f1b885cd56d9948f28dda810106147a16ff
Logan Quon <Logan Quon>
2026-02-19 21:39:27 -0800

    added comment

--- app/frontend/src/api/accounts.mjs
+++ app/frontend/src/api/accounts.mjs
@@ -2,7 +2,7 @@ const API_BASE_URL = import.meta.env.API_BASE_URL;
 import axios from "axios";
 
 export const getAllAccounts = () => {
-    //
+    // Note: NOT DONE YET.
     return axios.get(`${API_BASE_URL}/profile`, {
         headers: {
             'Content-Type': 'application/json'
