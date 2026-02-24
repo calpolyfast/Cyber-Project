2acef67e67b268e8df7ef374654f7f6041950199
Logan Quon <Logan Quon>
2026-02-24 13:58:26 -0800

    updated accounts info frontend

--- app/frontend/src/api/accounts.mjs
+++ app/frontend/src/api/accounts.mjs
@@ -1,11 +1,17 @@
 const API_BASE_URL = import.meta.env.API_BASE_URL;
 import axios from "axios";
 
-export const getAllAccounts = () => {
-    // Note: NOT DONE YET.
+export const getProfile = () => {
     return axios.get(`${API_BASE_URL}/profile`, {
         headers: {
             'Content-Type': 'application/json'
         }
     })
-};
+}
+
+export const updateAccount = (account) => {
+    return axios.post(`${API_BASE_URL}/deleteAccount`, account);
+}
+
+
+
