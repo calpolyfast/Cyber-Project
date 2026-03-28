8c6a72ba248af31b6fc731e0778c67b94e29f95b
Jonathan Woline <jonwoline@gmail.com>
2026-03-28 16:07:03 -0700

    Modified launcher app to be containerable

--- launcher/api/src/app.mjs
+++ launcher/api/src/app.mjs
@@ -3,23 +3,28 @@ import { spawn } from "child_process";
 import cors from "cors";
 import options from './cors_options.js';
 import { v4 } from 'uuid'
+import path from 'path'
+import { fileURLToPath } from 'url';
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 const app = express()
 const port = 4000
 
 app.use(cors(options))
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
-
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+app.use(express.static(path.join(__dirname, '../', 'build')));
+app.use((req, res, next) => {
+    if (req.path.startsWith('/api')) return next();
+    res.sendFile(path.join(__dirname, '../', 'build', 'index.html'));
+});
 
 app.post('/api/send-link-payload', (req, res) => {
     // For demo purposes. We might want to opt for haivng 'instanceID' be in a JWT that is created when the user deploys a chamber
     const { instance_id, payload } = req.body
 
-    const proc = spawn("bash", ["playwright/runplaywright.sh", instance_id, "gotolink.js", payload])
+    const proc = spawn("bash", ["../playwright/runplaywright.sh", instance_id, "gotolink.js", payload])
 
     proc.stdout.on("data", (data) => {
         if (res.headersSent)
@@ -46,13 +51,14 @@ app.get('/api/chamber/new', (req, res) => {
     const chamberId = v4();
     res.send(chamberId)
 
+    // TODO: Finish this
+    return;
     // const proc = spawn("bash", ["../../infra/chambers/deploy.sh", chamberId])
 
     proc.stdout.on("data", (data) => {
         if (res.headersSent)
             return
         console.log(data.toString())
-        proc.kill()
         return res.status(200).json({ data: data.toString() })
     })
 
