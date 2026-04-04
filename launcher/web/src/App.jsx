bd483c1bc1295250c83bb3bec9d3c6e7dc253c95
Theoden Melgar <theodenmelgar@gmail.com>
2026-04-03 21:56:34 -0700

    added AppContext to track chamber and flags, also added Flags page to track user progress

--- launcher/web/src/App.jsx
+++ launcher/web/src/App.jsx
@@ -1,22 +1,26 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
 import Home from "./pages/Home"
+import Labs from "./pages/Labs"
+import Flags from "./pages/Flags"
+import { AppProvider } from "./context/AppContext"
 
 function App() {
   return (
-    <main className="h-screen w-screen flex flex-row justify-stretch items-stretch overflow-y-auto">
-      <div className="flex bg-secondary h-full w-0 md:w-10"></div>
-      <div className="flex bg-white h-full w-20"></div>
-      <div className="flex flex-10">
-        <Router>
-            <Routes>
-                <Route index element={<Home />} />
-            </Routes>
-        </Router>
-      </div>
-      <div className="flex bg-white h-full w-20"></div>
-      <div className="flex bg-secondary h-full w-0 md:w-10"></div>
-    </main>
-    
+    <AppProvider>
+      <main className="h-screen w-screen flex flex-row justify-stretch items-stretch overflow-y-auto">
+        <div className="flex bg-primary h-full w-20"></div>
+        <div className="flex flex-1 bg-white text-gray-600">
+          <Router>
+              <Routes>
+                  <Route index element={<Home />} />
+                  <Route path="/labs" element={<Labs />} />
+                  <Route path="/flags" element={<Flags />} />
+              </Routes>
+          </Router>
+        </div>
+        <div className="flex bg-primary h-full w-20"></div>
+      </main>
+    </AppProvider>
   )
 }
 
--- launcher/web/src/pages/Home.jsx
+++ launcher/web/src/pages/Home.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 import { sendLinkPayload } from "../api/payloads.mjs";
 import { createChamber } from "../api/chambers.mjs";
 import { FaFlag, FaStoreAlt } from "react-icons/fa"
@@ -8,6 +9,7 @@ const Home = () => {
     const [ chamberID, setChamberID ] = useState("")
     const [ payload, setPayload ] = useState("")
     const [ resMessage, setResMessage ] = useState("")
+    const navigate = useNavigate()
 
     const handleDemoPayload = (e) => {
         e.preventDefault()
@@ -27,14 +29,14 @@ const Home = () => {
         }
     }
 
-    return <div className="flex flex-col justify-evenly w-full h-full py-8 bg-primary text-white">
+    return <div className="flex flex-col justify-evenly w-full h-full py-8 bg-white text-gray-800">
         <header className="flex flex-col items-center font-serif px-8 gap-4">
             <h1 className="text-6xl text-center"> FAST Cyber Pentesting Lab </h1>
             <h3 className="text-4xl text-center"> Cyber Security Research Team </h3>
         </header>
         <div className="flex justify-center">
             <button 
-                className="w-fit font-bold font-serif p-2 bg-secondary text-xl rounded-sm cursor-pointer
+                className="w-fit font-bold font-serif p-2 border border-secondary text-xl rounded-sm cursor-pointer
                             transform hover:scale-110 transition-transform duration-300 ease-in-out" 
                 onClick={handleCreateChamber}
             >
@@ -47,29 +49,33 @@ const Home = () => {
                     <div className="flex flex-col items-center gap-2 cursor-pointer
                                 transform hover:scale-105 transition-transform duration-300 ease-in-out">
                         <h3 className="text-3xl text-center font-serif"> Farm Store </h3>
-                        <div className="flex justify-center items-center h-30 md:h-35 lg:h-50 aspect-square bg-white rounded-md border border-gray-300">
+                        <div className="flex justify-center items-center h-30 md:h-35 lg:h-50 aspect-square bg-white rounded-md border border-secondary">
                             <div className="h-[80%] aspect-square">
-                                <FaStoreAlt className="text-black h-full w-full"/>
+                                <FaStoreAlt className="text-gray-700 h-full w-full"/>
                             </div>
                         </div>
                     </div>
                     <div className="flex flex-col items-center gap-2 cursor-pointer
-                            transform hover:scale-105 transition-transform duration-300 ease-in-out">
+                            transform hover:scale-105 transition-transform duration-300 ease-in-out"
+                            onClick={() => navigate("/store")}
+                    >
                         <h3 className="text-3xl text-center font-serif"> Labs </h3>
-                        <div className="flex justify-center items-center h-30 md:h-35 lg:h-50 aspect-square bg-white rounded-md border border-gray-300">
+                        <div className="flex justify-center items-center h-30 md:h-35 lg:h-50 aspect-square bg-white rounded-md border border-secondary">
                             <div className="h-[80%] aspect-square">
-                                <FaComputer className="text-black h-full w-full"/>
+                                <FaComputer className="text-gray-700 h-full w-full"/>
                             </div>
                         </div>
                     </div>
                 </div>
                 <div className="flex flex-row justify-center">
                     <div className="flex flex-col items-center gap-2 cursor-pointer
-                            transform hover:scale-105 transition-transform duration-300 ease-in-out">
+                            transform hover:scale-105 transition-transform duration-300 ease-in-out"
+                        onClick={() => navigate("/flags")}
+                    >
                         <h3 className="text-3xl text-center font-serif"> Check Flags </h3>
-                        <div className="flex justify-center items-center h-30 md:h-35 lg:h-50 aspect-square bg-white rounded-md border border-gray-300">
+                        <div className="flex justify-center items-center h-30 md:h-35 lg:h-50 aspect-square bg-white rounded-md border border-secondary">
                             <div className="h-[80%] aspect-square">
-                                <FaFlag className="text-black h-full w-full"/>
+                                <FaFlag className="text-gray-700 h-full w-full"/>
                             </div>
                         </div>
                     </div>
@@ -79,27 +85,31 @@ const Home = () => {
                 <div className="flex flex-col items-center gap-2 cursor-pointer
                             transform hover:scale-105 transition-transform duration-300 ease-in-out">
                     <h3 className="text-3xl text-center font-serif"> Farm Store </h3>
-                    <div className="flex justify-center items-center h-30 md:h-35 lg:h-50 aspect-square bg-white rounded-md border border-gray-300">
+                    <div className="flex justify-center items-center h-30 md:h-35 lg:h-50 aspect-square bg-white rounded-md border border-secondary">
                         <div className="h-[80%] aspect-square">
-                            <FaStoreAlt className="text-black h-full w-full"/>
+                            <FaStoreAlt className="text-gray-700 h-full w-full"/>
                         </div>
                     </div>
                 </div>
                 <div className="flex flex-col items-center gap-2 cursor-pointer
-                            transform hover:scale-105 transition-transform duration-300 ease-in-out">
+                            transform hover:scale-105 transition-transform duration-300 ease-in-out"
+                        onClick={() => navigate("/labs")}
+                >
                     <h3 className="text-3xl text-center font-serif"> Labs </h3>
-                    <div className="flex justify-center items-center h-30 md:h-35 lg:h-50 aspect-square bg-white rounded-md border border-gray-300">
+                    <div className="flex justify-center items-center h-30 md:h-35 lg:h-50 aspect-square bg-white rounded-md border border-secondary">
                         <div className="h-[80%] aspect-square">
-                            <FaComputer className="text-black h-full w-full"/>
+                            <FaComputer className="text-gray-700 h-full w-full"/>
                         </div>
                     </div>
                 </div>
                 <div className="flex flex-col items-center gap-2 cursor-pointer
-                            transform hover:scale-105 transition-transform duration-300 ease-in-out">
+                            transform hover:scale-105 transition-transform duration-300 ease-in-out"
+                        onClick={() => navigate("/flags")}
+                >
                     <h3 className="text-3xl text-center font-serif"> Check Flags </h3>
-                    <div className="flex justify-center items-center h-30 md:h-35 lg:h-50 aspect-square bg-white rounded-md border border-gray-300">
+                    <div className="flex justify-center items-center h-30 md:h-35 lg:h-50 aspect-square bg-white rounded-md border border-secondary">
                         <div className="h-[80%] aspect-square">
-                            <FaFlag className="text-black h-full w-full"/>
+                            <FaFlag className="text-gray-700 h-full w-full"/>
                         </div>
                     </div>
                 </div>
