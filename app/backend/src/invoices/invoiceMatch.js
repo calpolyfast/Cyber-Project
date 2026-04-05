05fe1af87336eb47a96d5f6b6a18c300b8182aab
Logan Quon <Logan Quon>
2026-04-05 16:41:45 -0700

    updated and added invoiveMatch file

--- app/backend/src/invoices/invoiceMatch.js
+++ app/backend/src/invoices/invoiceMatch.js
@@ -1,16 +1,19 @@
 import { getInvoiceByOrderId } from "./invoices.controllers"
 
 import { getProfile } from "..accounts/accounts.controllers"
-
+const express = require("express");
+const app = express();
 
 export function InvoiceMatch() {
     const UserInvoice = getInvoiceByOrderId;
     const userID = getProfile.userID;
-    return UserInvoice.userID == userID;
+    return UserInvoice.userID === userID;
 }
 
-export function detectVul() {
-    if (InvoiceMatch()) {
-        
-    }
+export function getVul() {
+    app.get('/api/user/profile', (res) => {
+        if (!InvoiceMatch()) {
+            return res.status(401).json("unauthorized");
+        }
+    })
 }
